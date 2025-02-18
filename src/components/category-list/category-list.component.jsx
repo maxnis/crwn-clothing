8b406a25211fa5584dcd07ee272558a499f392e8
Max <maxnis@gmail.com>
2025-02-18 15:48:41 -0500

    Create components, add font

---
+++ src/components/category-list/category-list.component.jsx
@@ -0,0 +1,14 @@
+import './category-list.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+const CategoryList = ({ data }) => {
+  return (
+    <div className="categories-container">
+      {data.map(category => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
+
+export default CategoryList;
